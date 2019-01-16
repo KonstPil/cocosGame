fd4ad4582500958f3966c891e880822d600b34ce
Arkanyga <Arkanyga1@mail.ru>
2019-01-16 19:11:03 +0500

    step 1
    переместил весь код в js папку, переписал начальный слой

--- js/game.js
+++ js/game.js
@@ -1,47 +1,46 @@
-var GameLayer = cc.Layer.extend({
-	ctor:function() {
-		this._super();
-		this.init();
-	},
-	init:function() {
-		this._super();
-		var size = cc.director.getWinSize();
-		
-		
-		var label = cc.LabelTTF.create("Hello World", "Arial", 40);
-	    label.setPosition(size.width / 2, size.height / 2);
-	    this.addChild(label, 1);
-		
-		var sprite = cc.Sprite.create("images/HelloWorld.png");
-		sprite.setPosition(size.width / 2, size.height / 2);
-		sprite.setScale(0.5);
-		this.addChild(sprite, 0);
-		
-	},
-	onEnter:function() {
-		this._super();
-	}
+let GameLayer = cc.Layer.extend({
+  ctor() {
+    this._super();
+    this.init();
+  },
 
-});
+  init() {
+    this._super();
+    //размер окна
+    let size = cc.director.getWinSize();
 
-GameLayer.scene = function() {
-	var scene = new cc.Scene();
-	var layer = new GameLayer();
-	scene.addChild(layer);
-	return scene;
+    let sprite = cc.Sprite.create("images/HelloWorld.png");//спрайт это картинка 
+    sprite.setPosition(size.width / 2, size.height / 2);
+    sprite.setScale(0.8);
+    this.addChild(sprite, 0);//0 это z-index для слоя 
+
+    let label = cc.LabelTTF.create("Hello World", "Arial", 40);//надпись 
+    label.setPosition(size.width / 2, size.height / 2);
+    this.addChild(label, 1);
+
+  },
+
+  onEnter() {
+    this._super();
+  },
+
+
+})
+
+GameLayer.scene = function () {
+  let scene = new cc.Scene();
+  let layer = new GameLayer();
+  scene.addChild(layer);
+  return scene;
 }
 
-window.onload = function(){
-	  cc.game.onStart = function(){
-		  //load resources
-		  cc.LoaderScene.preload(["images/HelloWorld.png"], function () {                  
-		   cc.director.runScene(GameLayer.scene());
-		  }, this);
-	  };
-	  cc.game.run("gameCanvas");
+
+window.onload = function () {
+  cc.game.onStart = function () {
+    //load resources
+    cc.LoaderScene.preload(["images/HelloWorld.png"], function () {
+      cc.director.runScene(GameLayer.scene());
+    }, this);
   };
-		  
-		  
-		  
-		  
-		  
+  cc.game.run("gameCanvas");
+};
