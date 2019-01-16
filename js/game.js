1082dd61db52efdf9d8dd429c03bb5da9d6ddfa8
Arkanyga <Arkanyga1@mail.ru>
2019-01-16 20:26:49 +0500

    step 3
    добавил прослушивание клика мышки по экрану

--- js/game.js
+++ js/game.js
@@ -24,7 +24,29 @@ let GameLayer = cc.Layer.extend({
 
   onEnter() {
     this._super();
+
+    cc.eventManager.addListener({
+      event: cc.EventListener.TOUCH_ONE_BY_ONE,
+      swallowTouches: true,
+      onTouchBegan: this.onTouchBegan,
+      onTouchMoved: this.onTouchMoved,
+      onTouchEnded: this.onTouchEnded
+    }, this)
   },
+
+  onTouchBegan(touch, event) {//при изменение размера экрана числовые значения на поле не будут менятся
+    let tp = touch.getLocation();//touchPoint
+    return true//если ставим true то дальше бдует считывать onTouchMoved и onTouchEnded, если false-нет
+  },
+
+  onTouchMoved(touch, event) {
+    let tp = touch.getLocation();//touchPoint
+  },
+
+  onTouchEnded(touch, event) {
+    let tp = touch.getLocation();//touchPoint
+  }
+
 
 
 })
